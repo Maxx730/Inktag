966e580e4c4336302e9a4d5bbebad4e92f497bf6
Maxx730 <max.kinghorn@gmail.com>
2018-04-20 09:59:38 -0400

    update

---
+++ app/src/main/java/com/squidswap/inktag/inktag/InktagActivity.java
@@ -0,0 +1,13 @@
+package com.squidswap.inktag.inktag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InktagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inktag);
+    }
+}
